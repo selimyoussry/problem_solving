0bfb874d0babb133637ed1fd981bd6ad2dc855d7
selim-macbook <selim@wearkinetic.com>
2016-02-11 17:21:12 -0500

    solved pramp interview

--- pramp/feb-13-interviewer/solution.py
+++ pramp/feb-13-interviewer/solution.py
@@ -26,4 +26,31 @@ class ShiftedArraySearch:
 
         return -1
 
-    
+    def find_sorted_shifted(self, arr, num, idx_start):
+
+        len_arr = len(arr)
+
+        if len_arr == 1:
+            return idx_start if arr[0] == num else -1
+
+        a = arr[0]
+        b = arr[-1]
+        mid_pos = len_arr / 2
+        x = arr[mid_pos]
+
+        if a < b:  # Then no shift
+            return self.find_sorted(arr, num, idx_start)
+
+        if num == x:
+            return idx_start + mid_pos
+
+        if x >= a:
+            if a <= num <= x:
+                return self.find_sorted_shifted(arr[:mid_pos], num, idx_start)
+            else:
+                return self.find_sorted_shifted(arr[mid_pos:], num, idx_start + mid_pos)
+        else:
+            if x <= num <= b:
+                return self.find_sorted_shifted(arr[mid_pos:], num, idx_start + mid_pos)
+            else:
+                return self.find_sorted_shifted(arr[:mid_pos], num, idx_start)
