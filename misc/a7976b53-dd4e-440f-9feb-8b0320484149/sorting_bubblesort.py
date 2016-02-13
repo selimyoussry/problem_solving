4d8ee4ace4bc62c815bfeb609d39119e6d719db8
selim-macbook <selim@wearkinetic.com>
2016-02-13 14:58:07 -0500

    done bubblesort

---
+++ misc/a7976b53-dd4e-440f-9feb-8b0320484149/sorting_bubblesort.py
@@ -0,0 +1,19 @@
+
+def swap(a, i, j):
+    tmp_a_i = a[i]
+    a[i] = a[j]
+    a[j] = tmp_a_i
+
+def bubble_sort(a):
+    lo = 0
+    hi = len(a) - 2
+    n_swaps = 1
+
+    while lo < hi and n_swaps > 0:
+        n_swaps = 0
+
+        for i in range(lo, hi + 1):
+            if a[i] > a[i + 1]:
+                swap(a, i, i+1)
+                n_swaps += 1
+        hi -= 1
