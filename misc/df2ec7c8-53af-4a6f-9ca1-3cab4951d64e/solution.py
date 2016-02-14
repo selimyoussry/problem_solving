2a0b329311cbb8ddb0a6d4a3d726e833a8e9f4c9
selim-macbook <selim@wearkinetic.com>
2016-02-14 14:59:51 -0500

    solved addie s too simple problem

--- misc/df2ec7c8-53af-4a6f-9ca1-3cab4951d64e/solution.py
+++ misc/df2ec7c8-53af-4a6f-9ca1-3cab4951d64e/solution.py
@@ -1,1 +1,11 @@
 # solution.py
+
+# Linear time solution, cannot do better
+
+def is_twice(arr):
+    s = set()
+    for elt in arr:
+        if elt not in s:
+            s.add(elt)
+        else:
+            return elt
