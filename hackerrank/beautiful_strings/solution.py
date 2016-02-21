3927c90b49c70f436319e6e70af13ef68fe8480f
selim-macbook <selim@wearkinetic.com>
2016-02-21 13:23:09 -0500

    solved beautiful strings suboptimal solution

--- hackerrank/beautiful_strings/solution.py
+++ hackerrank/beautiful_strings/solution.py
@@ -1,1 +1,35 @@
-# solution.py
+import sys
+
+
+class BeautifulString:
+
+    def __init__(self, s):
+        self.s = s
+        self.n = len(self.s)
+
+    @staticmethod
+    def remove(s, i, j):
+        assert i < j
+        return s[:i] + s[(i+1):j] + s[(j+1):]
+
+    def find_naive(self):
+        all = set()
+
+        for i in range(self.n):
+            for j in range(i + 1, self.n):
+                all.add(
+                    self.remove(self.s, i, j)
+                )
+
+        return all
+
+def reader():
+    for l in sys.stdin:
+        return l.strip()
+
+
+if __name__ == '__main__':
+    bs = BeautifulString(reader())
+    solutions = bs.find_naive()
+    print solutions
+    print len(solutions)
