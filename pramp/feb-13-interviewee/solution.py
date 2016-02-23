f5c7bf6ccdfe10f9faf0cbd1cfe30a1f2f8ca73d
selim-macbook <selim@wearkinetic.com>
2016-02-23 16:00:41 -0500

    added solution to pramp k-sort problem

--- pramp/feb-13-interviewee/solution.py
+++ pramp/feb-13-interviewee/solution.py
@@ -1,17 +1,50 @@
-# solution.py
+import heapq
 
-# This would have a worst case complexity n log(k)
 
-def insertion_sort_solution(a, k):
-    for j in range(1, len(a)):
-        i = j - 1
-        n_swaps = 0
-        while i >= 0 and a[i + 1] < a[i] and n_swaps <= k:
-            swap(a, i, i+1)
-            i -= 1
-            n_swaps += 1
+class SortingMethods:
+    def __init__(self):
+        pass
 
-def swap(a, i, j):
-    tmp_a_i = a[i]
-    a[i] = a[j]
-    a[j] = tmp_a_i
+    @staticmethod
+    def insertion_sort(a):
+        for i in range(1, len(a)):
+            j = i - 1
+            while j >= 0 and a[j + 1] < a[j]:
+                swap(a, j, j+1)
+                j -= 1
+
+    @staticmethod
+    def swap(a, i, j):
+        a_i = a[i]
+        a[i] = a[j]
+        a[j] = a_i
+
+    @staticmethod
+    def heap_sort(a):
+        out = []
+        heapq.heapify(a)
+        for i in range(len(a)):
+            out.append(heapq.heappop(a))
+
+        return out
+
+    @staticmethod
+    def k_optimal_heap_sort(a, k):
+        out = []
+
+        sliding_window = heapq.heapify(
+            a[:(k + 1)]
+        )
+
+        for elt in a[(k+1):]:
+            out.append(
+                heapq.heappop(sliding_window)
+            )
+            heapq.heappush(sliding_window, elt)
+
+        while len(sliding_window) > 0:
+            out.append(
+                heapq.heappop(sliding_window)
+            )
+
+        return out
